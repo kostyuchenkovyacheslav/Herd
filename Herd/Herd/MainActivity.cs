951ea64a05d97edc31a9b923d9a992607c12e67f
kostyuchenkovyacheslav <kostyuchenkovyacheslav@gmail.com>
2014-04-20 12:24:57 +0300

    Initial commit
    Initial commit

---
+++ Herd/Herd/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿namespace Herd
+{
+	using System;
+	using Android.App;
+	using Android.Content;
+	using Android.Runtime;
+	using Android.Views;
+	using Android.Widget;
+	using Android.OS;
+
+	[Activity (Label = "Herd")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
---
+++ Herd/Herd/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Herd
+{
+	using System.Threading;
+
+	using Android.App;
+	using Android.OS;
+
+	[Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			Thread.Sleep(10000); // Simulate a long loading process on app startup.
+			StartActivity(typeof(MainActivity));
+		}
+	}
+}
